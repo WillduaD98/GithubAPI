d2c97c1612adbdea48fdd0c927ce22442c3b8278
[William Duarte] <[willi_dds3@hotmail.com]>
2025-04-07 12:01:19 -0600

    adding token verification and env. data

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -7,4 +7,19 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  const auth = req.get('Authorization');
+  if (!auth ||auth.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token no proporcionado'})
+  }
+   const token = auth.split(' ')[1]
+  
+  try {
+    const key = process.env.JWT_SECRET_KEY || 'defectKey';
+    const payload = jwt.verify(token,key) as JwtPayload
+
+    req.user = payload
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'No valid Token' });
+  }
 };
--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,33 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const {username, password} = req.body
+
+  try {
+    const user = await User.findOne({ where: {username}})
+
+    if (!user) {
+      return res.status(401).json({message: 'User not found'});
+    };
+
+    const isValid = await bcrypt.compare(password, user.password)
+
+    if (!isValid) {
+      return res.status(401).json({message: 'Password not Valid'})
+    }
+
+    const payload = {username: user.username};
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY || 'defetValue',
+     {expiresIn: '1h'}
+    );
+    return res.json({token})
+
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: `Internal server Error`})
+  }
+
+  
 };
 
 const router = Router();
